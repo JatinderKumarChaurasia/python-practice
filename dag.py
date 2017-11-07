722a925d03f569f07f21c812bf3dab96950c7ef0
Alexandre Karpov <alexakarpov@gmail.com>
2017-11-07 13:44:51 -0500

    I think I'm done

--- dag.py
+++ dag.py
@@ -12,6 +12,19 @@ class Arc:
         self.destination = destination
         self.weight = weight
 
+    def __str__(self):
+        return "{0} -> {1}".format(self.origin, self.destination)
+
+    def __repr__(self):
+        return "{0} -> {1}".format(self.origin, self.destination)
+
+    def __eq__(self, other):
+        return self.origin == other.origin and self.destination == other.destination
+
+    def __hash__(self):
+        return hash(repr(self))
+
+
 def find_all_paths(from_node, to_node, in_graph):
     """
     Find all the paths from from_node to to_node in in_graph
@@ -20,27 +33,27 @@ def find_all_paths(from_node, to_node, in_graph):
     :param in_graph: list of tuples of the form (V1, V2, Weight)
     :return: list of paths
     """
-    all_paths = deque()  # how many hours did I waste because I missed you?!
+    all_paths = []
 
     def take(step, paths_queue):
-        src, dst, weight, history, visited = step
-        visited.add(dst)
+        curr_arc, history, visited = step
+        visited.add(curr_arc)
 
         # we took a step - it may lead us to victory! so need to write it down
-        history.append((src, dst, weight))
+        history.append(curr_arc)
 
-        if dst == to_node:
+        if curr_arc.destination == to_node:
             all_paths.append(history)
             return
 
-        new_edges = [(e[0], e[1], e[2], deepcopy(history), visited) for e in in_graph if
-                     (e[0] == dst and (e[1] not in visited or e[1] == to_node))]
+        new_edges = [(arc, deepcopy(history), visited) for arc in in_graph if
+                     (arc.origin == curr_arc.destination and (arc not in visited or arc.destination == to_node))]
         paths_queue.extend(new_edges)
 
         return
 
-    # from our graph, which is a list of edges, get all the edges that start at the "from" node
-    starting_paths = [(f, t, w, [], set(f)) for (f, t, w) in in_graph if f == from_node]
+    # get all the edges that start at the "from" node
+    starting_paths = [(arc, [], set(arc.origin)) for arc in in_graph if arc.origin == from_node]
     paths_queue = deque()
     paths_queue.extend(starting_paths)
     # ...and dive in
@@ -53,58 +66,71 @@ def find_all_paths(from_node, to_node, in_graph):
 
 if __name__ == "__main__":
     triangles = [
-        ("X", "Y", 2),
-        ("X", "Z", 9),
-        ("X", "V", 4),
-        ("Y", "V", 5),
-        ("V", "Z", 3),
-        ("Z", "Y", 6),
-        ("Z", "X", 11)
+        Arc("X", "Y", 2),
+        Arc("X", "Z", 9),
+        Arc("X", "V", 4),
+        Arc("Y", "V", 5),
+        Arc("V", "Z", 3),
+        Arc("Z", "Y", 6),
+        Arc("Z", "X", 11)
     ]
     # let's call this a test suite..
-    assert find_all_paths("X", "Z", triangles) == deque([[("X", "V", 4), ("V", "Z", 3)],
-                                                  [("X", "Z", 9)],
-                                                  [("X", "Y", 2), ("Y","V", 5), ("V", "Z", 3)]])
+    paths1 = find_all_paths("X", "Z", triangles)
+    print("Paths1:", paths1)
+    assert find_all_paths("X", "Z", triangles) == [[Arc("X", "V", 4), Arc("V", "Z", 3)],
+                                                   [Arc("X", "Z", 9)],
+                                                   [Arc("X", "Y", 2), Arc("Y","V", 5), Arc("V", "Z", 3)]]
 
-    assert find_all_paths("X", "X", triangles) == deque([[('X', 'V', 4), ('V', 'Z', 3), ('Z', 'X', 11)],
-                                                         [('X', 'Z', 9), ('Z', 'X', 11)],
-                                                         [('X', 'Y', 2), ('Y', 'V', 5), ('V', 'Z', 3), ('Z', 'X', 11)]])
+    paths_to_self = find_all_paths("X", "X", triangles)
+    assert paths_to_self == [[Arc("X", "V", 4), Arc("V", "Z", 3), Arc("Z", "X", 11)],
+                             [Arc("X", "Z", 9), Arc("Z", "X", 11)],
+                             [Arc("X", "Z", 9), Arc("Z", "Y", 6), Arc("Y", "V", 5), Arc("V","Z",3), Arc("Z","X", 11)],
+                             [Arc("X", "Y", 2), Arc("Y", "V", 5), Arc("V", "Z", 3), Arc("Z", "X", 11)]]
 
     # graph in the original problem
-    graph = [("A", "B", 5),
-             ("A", "E", 7),
-             ("A", "D", 5),
-             ("B", "C", 4),
-             ("C", "D", 8),
-             ("C", "E", 2),
-             ("D", "E", 6),
-             ("E", "B", 3),
-             ("D", "C", 8)]
+    graph = [Arc("A", "B", 5),
+             Arc("A", "E", 7),
+             Arc("A", "D", 5),
+             Arc("B", "C", 4),
+             Arc("C", "D", 8),
+             Arc("C", "E", 2),
+             Arc("D", "E", 6),
+             Arc("E", "B", 3),
+             Arc("D", "C", 8)]
 
     my_graph = [
-        ("A", "B", 5),
-        ("A", "E", 7),
-        ("A", "D", 5),
-        ("B", "C", 4),
-        ("C", "D", 8),
-        ("C", "E", 2),
-        ("D", "E", 6),
-        ("E", "B", 3),
-        ("D", "C", 8),
-        ("C", "A", 15)  # new edge from C back to A
+        Arc("A", "B", 5),
+        Arc("A", "E", 7),
+        Arc("A", "D", 5),
+        Arc("B", "C", 4),
+        Arc("C", "D", 8),
+        Arc("C", "E", 2),
+        Arc("D", "E", 6),
+        Arc("E", "B", 3),
+        Arc("D", "C", 8),
+        Arc("C", "A", 15)  # new edge from C back to A
     ]
 
-    paths = find_all_paths("A", "A", my_graph)
+    paths = find_all_paths("C", "C", my_graph)
     for path in paths:
         print("Path: {}".format(path))
 
     def paths_with_predicate(paths, predicate):
+        """
+        Half the problems presented will rely on the ability to filter paths by a predicate
+        :param paths:
+        :param predicate:
+        :return:
+        """
         return list(filter(lambda path: predicate(path), paths))
 
     def compute_path_cost(path):
+        """
+        just what it says
+        :param path:
+        :return:
+        """
         cost = 0
         for arc in path:
             cost = cost + path[2]
         return cost
-
-    print(compute_path_cost([("A", "B", 2), ("B", "C", 8)]))
